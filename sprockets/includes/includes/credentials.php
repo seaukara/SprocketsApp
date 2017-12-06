c51743d52dd04be12e5174ddb292e1ffdbfa8278
seaukara <karamanseau@gmail.com>
2017-12-06 13:56:55 -0800

    fix
    fix

--- sprockets/includes/includes/credentials.php
+++
@@ -1,7 +0,0 @@
-<?php
-//credentials.php
-
-define('DB_NAME','fearwildthings_db');
-define('DB_USER','bandmember_admin');
-define('DB_PASSWORD','ii_s@v3dUR_b35tFr13nd');
-define('DB_HOST','fearwildthings.thelandmarksband.com');
